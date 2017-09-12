71c052cb90a8d24a83b2dd7e1583d6b5fea0fb5b
Imran Khan <imran.khan@tenpearls.com>
2017-09-12 17:07:38 +0500

    new files added

---
+++ Migrations/DomainModelPostgreSql/20170907083015_testPG.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreMultipleProject.Migrations.DomainModelPostgreSql
+{
+    public partial class testPG : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SourceInfos",
+                columns: table => new
+                {
+                    SourceInfoId = table.Column<long>(type: "int8", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SourceInfos", x => x.SourceInfoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataEventRecords",
+                columns: table => new
+                {
+                    DataEventRecordId = table.Column<long>(type: "int8", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    SourceInfoId = table.Column<long>(type: "int8", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataEventRecords", x => x.DataEventRecordId);
+                    table.ForeignKey(
+                        name: "FK_DataEventRecords_SourceInfos_SourceInfoId",
+                        column: x => x.SourceInfoId,
+                        principalTable: "SourceInfos",
+                        principalColumn: "SourceInfoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataEventRecords_SourceInfoId",
+                table: "DataEventRecords",
+                column: "SourceInfoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataEventRecords");
+
+            migrationBuilder.DropTable(
+                name: "SourceInfos");
+        }
+    }
+}
