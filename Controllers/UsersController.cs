e23b0ce6d66d01e675a339d1272ffc1252f2620f
Imran Khan <imran.khan@tenpearls.com>
2017-09-22 12:23:17 +0500

    controllers and models

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DomainModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    public class UsersController : Controller
+    {
+		private readonly ILogger _logger;
+		private readonly IDataAccessProvider _dataAccessProvider;
+		public UsersController(IDataAccessProvider dataAccessProvider, ILogger<ValuesController> logger)
+		{
+			_dataAccessProvider = dataAccessProvider;
+			_logger = logger;
+		}
+		// GET: /<controller>/
+		public IActionResult Index()
+        {
+			_logger.LogInformation("Getting item without id");
+			var results = _dataAccessProvider.GetDataEventRecords().FirstOrDefault();
+			return View(results);
+        }
+    }
+}
---
+++ Models/ResultsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DomainModel.Model;
+using System;
+
+namespace TodoApi.Models
+{
+    public class ResultsViewModel
+    {
+		public long DataEventRecordId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime Timestamp { get; set; }
+		public SourceInfoViewModel SourceInfo { get; set; }
+		public long SourceInfoId { get; set; }
+	}
+	public class SourceInfoViewModel
+	{
+		public long SourceInfoId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public DateTime Timestamp { get; set; }
+		public List<ResultsViewModel> DataEventRecords { get; set; }
+	}
+
+}
